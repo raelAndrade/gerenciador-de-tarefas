a994632b24aad8eb266b1defcedd0b84d9e1b136
Israel Andrade <israelg.andrade@bol.com.br>
2020-11-11 19:29:04 -0300

    Correção de query JPQL

--- src/main/java/br/com/demo/repository/TarefaRepository.java
+++ src/main/java/br/com/demo/repository/TarefaRepository.java
@@ -15,9 +15,9 @@ public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 	@Query("SELECT t FROM Tarefa t WHERE t.usuario.email = :emailUsuario ORDER BY t.id DESC") // JPQL - Linguagem de consulta da API de Persistencia do Java
 	Page<Tarefa> carregarTarefasPorUsuario(@Param("emailUsuario") String email, Pageable pageable);
 	
-	@Query("SELECT count(*) FROM Tarefa t WHERE t.usuario.email = :emailUsuario AND t.concluida = 1")
+	@Query("SELECT count(*) FROM Tarefa t WHERE t.usuario.email = :emailUsuario AND t.concluida is true")
 	int quantidadeTarefasConcluida(@Param("emailUsuario") String email);
 	
-	@Query("SELECT count(*) FROM Tarefa t WHERE t.usuario.email = :emailUsuario AND t.concluida = 0")
+	@Query("SELECT count(*) FROM Tarefa t WHERE t.usuario.email = :emailUsuario AND t.concluida is false")
 	int quantidadeTarefasNaoConcluida(@Param("emailUsuario") String email);
 }
