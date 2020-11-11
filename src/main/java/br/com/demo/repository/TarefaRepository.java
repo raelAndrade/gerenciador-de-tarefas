97283e7dc3ee17eba8d468f237d620e187a1da3a
Israel Andrade <israelg.andrade@bol.com.br>
2020-11-11 19:12:10 -0300

    Removendo anotação transactional

--- src/main/java/br/com/demo/repository/TarefaRepository.java
+++ src/main/java/br/com/demo/repository/TarefaRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.com.demo.model.Tarefa;
 
 @Repository
-@Transactional
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 	
 	@Query("SELECT t FROM Tarefa t WHERE t.usuario.email = :emailUsuario ORDER BY t.id DESC") // JPQL - Linguagem de consulta da API de Persistencia do Java
